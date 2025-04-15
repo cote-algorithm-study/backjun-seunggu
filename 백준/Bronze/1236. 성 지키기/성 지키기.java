4427aca0818e9f308bc889613bdb2e5b3b74cd57
leeseunggu <32692807+seunggulee1007@users.noreply.github.com>
2025-04-15 14:57:57 +0900

    fix: width , height 헷갈리는 변수명 rowCount , colCount로 정정

--- 백준/Bronze/1236. 성 지키기/성 지키기.java
+++ 백준/Bronze/1236. 성 지키기/성 지키기.java
@@ -16,27 +16,27 @@ public class Main {
 
     private static int solution(BufferedReader reader) throws IOException {
         int[] widthHeight = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int width = widthHeight[0];
-        int height = widthHeight[1];
-        String[][] map = getCastle(reader, width, height);
+        int rowCount = widthHeight[0];
+        int colCount = widthHeight[1];
+        String[][] map = getCastle(reader, rowCount, colCount);
         int row = 0;
-        row = getRowCount(width, map, row);
-        int col = getColCount(width, height, map);
+        row = getRowCount(rowCount, map, row);
+        int col = getColCount(rowCount, colCount, map);
         int result = Math.max(row, col);
         return result;
     }
 
-    private static String[][] getCastle(BufferedReader reader, int width, int height) throws IOException {
-        String[][] map = new String[width][height];
-        for(int i = 0; i < width; i++) {
+    private static String[][] getCastle(BufferedReader reader, int rowCount, int colCount) throws IOException {
+        String[][] map = new String[rowCount][colCount];
+        for(int i = 0; i < rowCount; i++) {
             String line = reader.readLine();
             map[i] = line.split("");
         }
         return map;
     }
 
-    private static int getRowCount(int width, String[][] map, int row) {
-        for(int i = 0; i< width; i++) {
+    private static int getRowCount(int rowCount, String[][] map, int row) {
+        for(int i = 0; i< rowCount; i++) {
             if(isContainsX(map[i])) {
                 continue;
             }
@@ -45,10 +45,10 @@ public class Main {
         return row;
     }
 
-    private static int getColCount(int width, int height, String[][] map) {
+    private static int getColCount(int rowCount, int colCount, String[][] map) {
         int col = 0;
-        e: for(int i = 0; i< height; i++) {
-            for(int j = 0; j< width; j++) {
+        e: for(int i = 0; i< colCount; i++) {
+            for(int j = 0; j< rowCount; j++) {
                 if("X".equals(map[j][i])) {
                     continue e;
                 }
