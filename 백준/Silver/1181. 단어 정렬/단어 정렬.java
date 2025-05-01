701e039b3f7befa0b862a0f1460f77065da96ee7
leeseunggu <32692807+seunggulee1007@users.noreply.github.com>
2025-05-01 15:50:18 +0900

    [Silver V] Title: 단어 정렬, Time: 360 ms, Memory: 26432 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String result = solution(reader);
+            System.out.printf("%s", result);
+        } catch (IOException e) {
+
+        }
+    }
+
+    private static String solution(BufferedReader reader) throws IOException {
+        int count = Integer.parseInt(reader.readLine());
+        Set<String> alphabets = new LinkedHashSet<>();
+        for (int i = 0; i < count; i++) {
+            alphabets.add(reader.readLine());
+        }
+        List<String> alphabetList = new ArrayList<>(alphabets);
+        Collections.sort(alphabetList, ((o1, o2) -> {
+            if (o1.length() == o2.length()) {
+                return o1.compareTo(o2);
+            }
+            return o1.length() - o2.length();
+        }));
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String alphabet : alphabetList) {
+            stringBuilder.append(alphabet).append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
+}
